a4bc1c24f8277a53e5dda15aae803ccc3042f09b
Vishesh Vijayvargiya <vijay.vishu03@gmail.com>
2023-12-18 16:01:05 +0530

    pages initialised

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello this is our streaming app!!! 
-    </main>
+   <p className='text-red-500 font-bold'>
+      Hello luminar frontend
+   </p>
   )
 }
